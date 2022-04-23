63a85fa76ef0313eb4fe841835688eae43d9faf6
Sir Code <taylorschneider07@yahoo.com>
2022-04-23 19:39:36 -0400

    Create window frame

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
-
+	private static void createAndShowGUI() {
+	    JFrame frame = new JFrame("FrameDemo");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	    JLabel emptyLabel = new JLabel("");
+	    emptyLabel.setPreferredSize(new Dimension(175, 100));
+	    frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+	    frame.pack();
+	    frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
