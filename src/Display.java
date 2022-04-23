02aee403717af2496731d18c782565e293f660e7
Sir Code <taylorschneider07@yahoo.com>
2022-04-23 19:57:02 -0400

    Create threaded classes

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,14 @@
+import java.awt.Canvas;
+import javax.swing.JFrame;
 
-public class Display {
-
+public class Display extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+	
+	private Thread thread;
+	private JFrame frame;
+	
+	@Override
+	public void run() {
+		
+	}
 }
