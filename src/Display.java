e98cc8f5d6fbd4e3e4f3ef898cd4b419bbedab01
Sir Code <taylorschneider07@yahoo.com>
2022-04-23 20:02:26 -0400

    Update Display.java

--- src/Display.java
+++ src/Display.java
@@ -1,4 +1,6 @@
 import java.awt.Canvas;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Display extends Canvas implements Runnable {
@@ -6,6 +8,17 @@ public class Display extends Canvas implements Runnable {
 	
 	private Thread thread;
 	private JFrame frame;
+	private static String title = "Open Marcher";
+	private static final int WIDTH = 768;
+	private static final int HEIGHT = 512;
+	private static boolean rendering = false;
+	
+	public Display() {
+		this.frame = new JFrame();
+		
+		Dimension size = new Dimension(WIDTH, HEIGHT);
+		this.setPreferredSize(size);
+	}
 	
 	@Override
 	public void run() {
